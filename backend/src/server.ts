d42ec182d49bb23b829520ed7e22525b096651cf
Vitor Bertti <berttivitor@gmail.com>
2020-06-02 16:24:11 -0300

    Revert "Created database"
    This reverts commit 4260be4a58bc520dd9b1f8f3f2e09c6def109178.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,9 @@
 import express from 'express'
-import routes from './routes'
 
 const app = express();
-app.use(express.json());
-app.use(routes);
+
+app.get('/users', (req, res) => {
+   res.json([{}]);
+});
 
 app.listen(3333, () => console.log('server is running!'));
