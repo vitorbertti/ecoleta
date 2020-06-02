4260be4a58bc520dd9b1f8f3f2e09c6def109178
Vitor Bertti <berttivitor@gmail.com>
2020-06-02 16:20:49 -0300

    Created database

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,8 @@
 import express from 'express'
+import routes from './routes'
 
 const app = express();
-
-app.get('/users', (req, res) => {
-   res.json([{}]);
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => console.log('server is running!'));
