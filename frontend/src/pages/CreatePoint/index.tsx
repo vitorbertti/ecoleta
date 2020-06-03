584acd0c2d1d8bbed267e0cffa5c7518a57096d8
Vitor Bertti <berttivitor@gmail.com>
2020-06-03 18:26:22 -0300

    Added map and get api calls

--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -1,11 +1,70 @@
-import React from 'react'
+import React, { useEffect, useState, ChangeEvent } from 'react'
 import { Link } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
+import { Map, TileLayer, Marker } from 'react-leaflet'
+import axios from 'axios'
 
+import api from '../../services/api'
 import './styles.css'
 import logo from '../../assets/logo.svg'
 
+interface Item {
+   id: number;
+   title: string;
+   image_url: string;
+}
+
+interface IBGEUFResponse {
+   sigla: string;
+}
+
+interface IBGECityResponse {
+   nome: string;
+}
+
 const CreatePoint: React.FC = () => {
+
+   const [items, setItems] = useState<Item[]>([]);
+   const [ufs, setUfs] = useState<string[]>([]);
+   const [selectedUf, setSelectedUf] = useState('0');
+   const [cities, setCities] = useState<string[]>([]);
+   const [selectedCity, setSelectedCity] = useState('0');
+
+   useEffect(() => {
+      api.get('/items').then(response => {
+         setItems(response.data);
+      })
+   }, []);
+
+   useEffect(() => {
+      axios.get<IBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados').then(response => {
+         const ufInitials = response.data.map(uf => uf.sigla);
+         setUfs(ufInitials);
+      })
+   }, []);
+
+   useEffect(() => {
+      if(selectedUf === '0') {
+         return;
+      }
+      axios.get<IBGECityResponse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`).then(response => {
+         const cityNames = response.data.map(city => city.nome);
+         setCities(cityNames);
+      })
+
+   }, [selectedUf]);
+
+   function handleSelectedUf(e: ChangeEvent<HTMLSelectElement>) {
+      const uf = e.target.value;
+      setSelectedUf(uf); 
+   }
+
+   function handleSelectedCity(e: ChangeEvent<HTMLSelectElement>) {
+      const city = e.target.value;
+      setSelectedCity(city); 
+   }
+
+
    return (
       <div id="page-create-point">
          <header>
@@ -43,18 +102,32 @@ const CreatePoint: React.FC = () => {
                   <h2>Endereço</h2>
                   <span>Selecione o endereço no mapa</span>
                </legend>
+
+               <Map center={[ -23.5673053, -46.7040718 ]} zoom={15}>
+                  <TileLayer 
+                     attribution="&amp;copy <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors" 
+                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  />
+                  <Marker position={[ -23.5673053, -46.7040718 ]} />
+               </Map>
                
                <div className="field-group">
                   <div className="field">
                      <label htmlFor="uf">Estado (UF):</label>
-                     <select name="uf" id="uf">
+                     <select name="uf" id="uf" value={selectedUf} onChange={handleSelectedUf}>
                         <option value="0">Selecione uma UF</option>
+                        {ufs.map(uf => (
+                           <option key={uf} value={uf}>{uf}</option>
+                        ))}
                      </select>
                   </div>
                   <div className="field">
                      <label htmlFor="city">Cidade:</label>
-                     <select name="city" id="city">
+                     <select name="city" id="city" value={selectedCity} onChange={handleSelectedCity}>
                         <option value="0">Selecione uma Cidade</option>
+                        {cities.map(city => (
+                           <option key={city} value={city}>{city}</option>
+                        ))}
                      </select>
                   </div>
                </div>
@@ -66,30 +139,13 @@ const CreatePoint: React.FC = () => {
                   <span>Selecione um ou mais itens abaixo</span>
                </legend>
                <ul className="items-grid">
-                  <li className="selected">
-                     <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                     <span>Óleo de cozinha</span>
-                  </li>
-                  <li>
-                     <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                     <span>Óleo de cozinha</span>
-                  </li>
-                  <li>
-                     <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                     <span>Óleo de cozinha</span>
-                  </li>
-                  <li>
-                     <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                     <span>Óleo de cozinha</span>
-                  </li>
-                  <li>
-                     <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                     <span>Óleo de cozinha</span>
-                  </li>
-                  <li>
-                     <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                     <span>Óleo de cozinha</span>
-                  </li>
+                  {items.map(item => (
+                     <li key={item.id}>
+                        <img src={item.image_url} alt={item.title}/>
+                        <span>{item.title}</span>
+                     </li>
+                  ))}
+                  
                </ul>
             </fieldset>
             <button type="submit">Cadastrar ponto de coleta</button>
