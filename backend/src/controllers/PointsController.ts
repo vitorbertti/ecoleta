281b8a3dc59b0485180da570fa5f1434e148888d
Vitor Bertti <berttivitor@gmail.com>
2020-06-05 18:10:13 -0300

    Added image upload

--- backend/src/controllers/PointsController.ts
+++ backend/src/controllers/PointsController.ts
@@ -1,5 +1,7 @@
 import { Request, Response, request } from 'express'
 import knex from '../database/connection'
+import dotenv from 'dotenv'
+dotenv.config();
 
 class PointsController {
    async index(req: Request, res: Response) {
@@ -17,8 +19,15 @@ class PointsController {
          .distinct()
          .select('points.*');
 
+      const serializedPoints = points.map(point => {
+         return {
+            ...point,
+            image_url: `${process.env.UPLOADS_URL}${point.image}`,
+         };
+      });
 
-      return res.json(points)
+
+      return res.json(serializedPoints)
       
    }
 
@@ -28,7 +37,7 @@ class PointsController {
       const trx = await knex.transaction();
 
       const point = { 
-         image: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+         image: req.file.filename,
          name, 
          email, 
          whatsapp, 
@@ -42,7 +51,10 @@ class PointsController {
    
       const point_id = insertedIds[0];
    
-      const pointItems = items.map((item_id: number) => {
+      const pointItems = items
+         .split(',')
+         .map((item: string) => Number(item.trim()))
+         .map((item_id: number) => {
          return {
             item_id,
             point_id,
@@ -65,12 +77,17 @@ class PointsController {
          return res.status(400).json({ message: 'Point not found' });
       }
 
+      const serializedPoint = {
+            ...point,
+            image_url: `${process.env.UPLOADS_URL}${point.image}`,
+      };
+
       const items = await knex('items')
          .join('point_items', 'items.id', '=', 'point_items.id')
          .where('point_items.point_id', id)
          .select('items.title');
 
-      return res.json({point, items});
+      return res.json({point: serializedPoint, items});
    }
 }
 
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express'
+import multer from 'multer'
+import multerConfig from './config/multer'
 
 import ItemsController from './controllers/ItemsController'
 import PointsController from './controllers/PointsController'
 
 const routes = Router();
+const upload = multer(multerConfig);
 
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
@@ -11,7 +14,7 @@ const itemsController = new ItemsController();
 routes.get('/items', itemsController.index);
 
 routes.get('/points', pointsController.index);
-routes.post('/points', pointsController.store);
+routes.post('/points', upload.single('image'), pointsController.store);
 routes.get('/points/:id', pointsController.show);
 
 export default routes;
